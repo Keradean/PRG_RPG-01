c28ebca1b22e9d73f39f2fbf488a85713706a170
Dennis De Col <dennis.decol@gmx.net>
2025-04-15 11:34:34 +0200

    Updated BeaseCharacterController ADD Cinemmaschine

--- Assets/Script/BaseCharacterController.cs
+++ Assets/Script/BaseCharacterController.cs
@@ -1,13 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.InputSystem.InputAction;
 
 public class BaseCharacter : MonoBehaviour
-{  
-    public void Movement(CallbackContext ctx) 
+{
+    private Vector2 movementInput;
+    [SerializeField] float movementSpeed;
+    public void Movement(CallbackContext ctx)
     {
-        
-        Debug.Log($"Context{ctx.ReadValue<Vector2>()}");
+
+        movementInput = ctx.ReadValue<Vector2>();
+
+
+        //var value = ctx.ReadValue<Vector2>();
+        //transform.position += new Vector3(ctx.ReadValue<Vector2>().x, ctx.ReadValue<Vector2>().y,0) * Time.deltaTime;
+        // Debug.Log($"Context{ctx.ReadValue<Vector2>()}");
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed;
+
     }
 }
